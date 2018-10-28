b01d34e09d164937d9a3f911b6d2f91b49485010
karthikhr31 <karthikhr41@gmail.com>
2018-10-28 16:44:38 +0530

    date page commit

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+setInterval(function(){
+
+
 ReactDOM.render(
-<h1>Welcome KARTHIK how are u  </h1>,
+<h1>The current time is {new Date().toLocaleTimeString()}</h1>,
 document.getElementById('root')
 	);
+},1000);
