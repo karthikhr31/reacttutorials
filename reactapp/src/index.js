5d469d630becedba60319678aeb45e7f484a8f31
karthikhr31 <karthikhr41@gmail.com>
2018-11-01 23:39:58 +0530

    list and map implementation

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,37 +1,15 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 
-function Message(props){
-	if(props.value){
-		return <h1>I am First message</h1>
-	}
-		return <h1>This is second message</h1>
-}	
-
-class Btn extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {value : true}
-	}
-	handleClick = () =>{
-		this.setState({
-			value : !this.state.value
-		})
-	}
-	render(){
-		return(
-			<div>
-		 <button onClick = {this.handleClick}>change</button>
-		 <Message value={this.state.value}></Message>
-		 </div>
-			)
-	}
+function GetIndividuals(props){
+	const namearray=props.nameValue
+	return <ul>{
+namearray.map((namearray,index)=><li key={index}>{namearray}</li>)
+	}</ul>
 }
+const list =  ['karthik','chethu','muthu','varun']
 
-
-
-
-ReactDom.render(
-<Btn/>,
+ReactDOM.render(
+<GetIndividuals nameValue = {list}/>,
 document.getElementById('root')
-)
+	)
