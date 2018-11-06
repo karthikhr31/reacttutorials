4017840f31b980153c47541eb5714a45ce8cce1f
karthikhr31 <karthikhr41@gmail.com>
2018-11-06 21:45:34 +0530

    form data

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function GetIndividuals(props){
-	const namearray=props.nameValue
-	return <ul>{
-namearray.map((namearray,index)=><li key={index}>{namearray}</li>)
-	}</ul>
-}
-const list =  ['karthik','chethu','muthu','varun']
+class Test extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {value :'',checkvalue :'false'}
 
+	}
+
+valueSubmit=(e)=>{
+	console.log(this.state.value)
+	e.preventDefault()
+}
+
+valueChange=(e)=>{
+this.setState({
+	value : e.target.value,
+	checkvalue : !this.state.checkvalue
+})
+}
+
+
+	render(){
+		return (
+<form onSubmit={this.valueSubmit}>
+<input type='text' value={this.state.value} onChange={this.valueChange}/>
+<input type='checkbox' value={this.state.checkvalue} onChange={this.valueChange}/>
+<input type='submit' value= 'change'/>
+</form>
+
+			)
+	}
+}
 ReactDOM.render(
-<GetIndividuals nameValue = {list}/>,
+<Test/>,
 document.getElementById('root')
 	)
